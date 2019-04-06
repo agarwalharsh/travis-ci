7650cfdfd0ce96bbd4b9137710853c066486f9f7
Harsh Agarwal <haragarw@publicisgroupe.net>
2019-04-06 15:06:02 +0530

    Added build

---
+++ build/precache-manifest.d538a82b75a917334158bf0848859514.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dfe0cc15bc15717c13e2",
+    "url": "/static/js/main.b8ba0b6e.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "dfe0cc15bc15717c13e2",
+    "url": "/static/css/main.7cbea50d.chunk.css"
+  },
+  {
+    "revision": "0c22ebe07c15c90beac41848c0b678f5",
+    "url": "/index.html"
+  }
+];
